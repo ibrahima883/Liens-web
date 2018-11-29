5ff99636879fcb573ffdbfb45fbb880a90f7c7bb
Ibrahima <ibrahimadiallo883@gmail.com>
2018-11-29 08:17:16 +0100

    Add files via upload

--- js/liensweb.js
+++ js/liensweb.js
@@ -117,10 +117,10 @@ ajouterLienBtn.addEventListener("click", function (e) {
     urlElt.setAttribute("style", "margin-left: 10px; width: 250px;");
     
     // Création du bouton d'ajout du lien aux autres liens de la page
-    var ajouterBtn = document.createElement("button"); // button ou input
-    ajouterBtn.textContent = "Ajouter"; // si button
-    /*ajouterBtn.type = "submit";
-    ajouterBtn.value = "Ajouter";*/
+    var ajouterBtn = document.createElement("input"); // button ou input
+    //ajouterBtn.textContent = "Ajouter"; // si button
+    ajouterBtn.type = "submit";
+    ajouterBtn.value = "Ajouter";
     ajouterBtn.setAttribute("style", "margin-left: 10px;");
     
     // Ajout des éléments "input" et du nouveau boutton créés dans le formulaire
@@ -133,62 +133,54 @@ ajouterLienBtn.addEventListener("click", function (e) {
     pElt.replaceChild(formElt, ajouterLienBtn);        
         
     // Gestionnaire d'ajout du lien créé sur la page
-    ajouterBtn.addEventListener("click", function (e) { 
+    formElt.addEventListener("submit", function (e) { 
+        
+        e.preventDefault();
         
         // Les variables d'infos du lien
         var auteur = auteurElt.value;
         var titre = titreElt.value;
         var url = urlElt.value;
 
-        // Si l'un des champs de saisie (tous obligatoires) n'a pas été renseigné, l'ajout
-        // du lien est annulé et on indique à l'utilisateur qu'il doit le renseigner
-        if ( (auteur === "") || (titre === "") || (url === "") ) {
-            
-            // Gestionnaire de validation de l'ajout
-            formElt.addEventListener("submit", function () {}); // "submit" ou "input"
-            
-        } else { // Tous les champs de saisie ont été renseignés
-
-            // Ajoute "http://" au début de l'url si elle ne commence ni par "https://", "http://", "ftp://" ou "ftps://"
-            if ( !(url.startsWith("http://")) && !(url.startsWith("https://")) && !(url.startsWith("ftp://")) && !(url.startsWith("ftps://")) ) {
-                url = "http://" + url;
-            }
-
-            // Création d'un élément "p" pour le message d'information
-            var infoMsg = document.createElement("p");
-
-            // Expression régulière pour vérifier la validité de l'url
-            var regex = /^(http:\/\/www\.|https:\/\/www\.|http:\/\/|https:\/\/|ftp:\/\/|ftps:\/\/)?[a-z0-9]+([\-\.]{1}[a-z0-9]+)*\.[a-z]{2,5}(:[0-9]{1,5})?(\/.*)?$/;
-
-            // Ajout du lien ou non selon le résulatat de la vérification
-            if (regex.test(url)) {
-
-                // Ajout du nouveau lien sur la page
-                ajouterLien(titre, url, auteur);
-
-                // Message de confirmation de l'ajout du lien
-                infoMsg.textContent = 'Le lien "' + titre + '" a bien été ajouté.';
-                infoMsg.setAttribute("style", "background-color: rgb(214, 236, 246); padding: 20px 10px;"); 
-                // color: rgba(0, 0, 0, 0.7); background-color: rgba(45, 154, 206, 0.2)
-
-            } else {
-
-                // Le message à afficher pour informer l'utilisateur
-                infoMsg.textContent = 'Le lien "' + titre + " n'a pas été ajouté car l'url est invalide.";
-                infoMsg.setAttribute("style", "background-color: rgba(237, 71, 104, 0.5); padding: 20px 10px;");
-
-            }              
-
-            // Fait réapparaître le bouton "Ajouter le lien " à la place du formulaire 
-            pElt.replaceChild(ajouterLienBtn, formElt);             
-
-            // Affiche le message d'information
-            document.body.insertBefore(infoMsg, pElt);
-
-            // Fait disparaître le message au bout de 2 secondes
-            setTimeout(function () {
-                document.body.removeChild(infoMsg);
-            }, 2000);
+        // Ajoute "http://" au début de l'url si elle ne commence ni par "https://", "http://", "ftp://" ou "ftps://"
+        if ( !(url.startsWith("http://")) && !(url.startsWith("https://")) && !(url.startsWith("ftp://")) && !(url.startsWith("ftps://")) ) {
+            url = "http://" + url;
         }
+
+        // Création d'un élément "p" pour le message d'information
+        var infoMsg = document.createElement("p");
+
+        // Expression régulière pour vérifier la validité de l'url
+        var regex = /^(http:\/\/www\.|https:\/\/www\.|http:\/\/|https:\/\/|ftp:\/\/|ftps:\/\/)?[a-z0-9]+([\-\.]{1}[a-z0-9]+)*\.[a-z]{2,5}(:[0-9]{1,5})?(\/.*)?$/;
+
+        // Ajout du lien ou non selon le résulatat de la vérification
+        if (regex.test(url)) {
+
+            // Ajout du nouveau lien sur la page
+            ajouterLien(titre, url, auteur);
+
+            // Message de confirmation de l'ajout du lien
+            infoMsg.textContent = 'Le lien "' + titre + '" a bien été ajouté.';
+            infoMsg.setAttribute("style", "background-color: rgb(214, 236, 246); padding: 20px 10px;"); 
+            // color: rgba(0, 0, 0, 0.7); background-color: rgba(45, 154, 206, 0.2)
+
+        } else {
+
+            // Le message à afficher pour informer l'utilisateur
+            infoMsg.textContent = 'Le lien "' + titre + " n'a pas été ajouté car l'url est invalide.";
+            infoMsg.setAttribute("style", "background-color: rgba(237, 71, 104, 0.5); padding: 20px 10px;");
+
+        }              
+
+        // Fait réapparaître le bouton "Ajouter le lien " à la place du formulaire 
+        pElt.replaceChild(ajouterLienBtn, formElt);             
+
+        // Affiche le message d'information
+        document.body.insertBefore(infoMsg, pElt);
+
+        // Fait disparaître le message au bout de 2 secondes
+        setTimeout(function () {
+            document.body.removeChild(infoMsg);
+        }, 2000);
     });  
 });
